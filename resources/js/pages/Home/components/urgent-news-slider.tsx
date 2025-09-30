65dbb34a73f6625596a669e3183d77a56c41c976
Hussain <hussainammar12@gmail.com>
2025-10-01 00:17:13 +0300

    fix urgen news link

--- resources/js/pages/Home/components/urgent-news-slider.tsx
+++ resources/js/pages/Home/components/urgent-news-slider.tsx
@@ -115,7 +115,7 @@ export default function UrgentNewsSlider({
                                             </span>
                                             
                                             <Link
-                                                href={`/news/${news.slug}`}
+                                                href={`/news/${news.id}`}
                                                 className={`inline-flex items-center gap-2 text-red-600 hover:text-red-700 text-sm font-medium transition-colors ${isRTL ? 'flex-row-reverse font-arabic' : ''}`}
                                             >
                                                 <span>{t('news.readMore')}</span>
