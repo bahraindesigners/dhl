64e3eac43e395fad5a48a717ed94daf555d62c8e
Hussain <hussainammar12@gmail.com>
2025-10-02 17:43:08 +0300

    update fav icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,8 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/public/uinuon-logo.jpeg" type="image/jpeg">
+        <link rel="apple-touch-icon" href="/public/uinuon-logo.jpeg">
 
         {{-- Preconnect to Google Fonts for better performance --}}
         <link rel="preconnect" href="https://fonts.googleapis.com">
